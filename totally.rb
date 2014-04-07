dbe44a99942dea1baa24879dbbebe829ede4bc8f
Tushar Pokle <tushar.pokle@gmail.com>
2014-04-07 23:08:30 +1000

    Totally 0.4.0

--- totally.rb
+++ totally.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Totally < Formula
   homepage "https://github.com/pokle/totally"
-  url "https://github.com/pokle/totally/archive/0.3.0.tar.gz"
-  sha1 "6700b4a8a7a18af963c4f47b52a626718cdd555a"
+  url "https://github.com/pokle/totally/archive/0.4.0.tar.gz"
+  sha1 "6dcec7f74fa35831037adde936a0fa8ea36c6b13"
   head "https://github.com/pokle/totally.git"
 
   def install
