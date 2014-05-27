57d9be091f02207acc7efd173ebbeabd73154f5b
Tushar Pokle <tushar.pokle@gmail.com>
2014-05-27 21:30:55 +1000

    another attempt

--- totally.rb
+++ totally.rb
@@ -8,7 +8,7 @@ class Totally < Formula
 
   def install
     system './scripts/package'
-    system 'echo "function cmd_version(){ echo 0.6.0; }" >> bin/totally'
+    system 'sed -ie "s/^.*cmd_version.*$/function cmd_version(){ echo 0.6.0; }/" bin/totally'
     bin.install "bin/totally"
   end
 
