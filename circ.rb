bd0cb9bb0e3fe8952c6df04df4f788f23bba382b
Tushar Pokle <tushar.pokle@gmail.com>
2014-07-28 23:06:43 +1000

    Fixed up github refernce for circ

--- circ.rb
+++ circ.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Circ < Formula
   homepage "https://github.com/pokle/circ"
-  head "https://github.com/pokle/totally.git"
+  head "https://github.com/pokle/circ.git"
 
   def install
     bin.install "circ"
